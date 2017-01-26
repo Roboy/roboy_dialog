4751599d6e8f53d2c1ad6ba23f80db959eaacd75
letrend <simon.trendel@tum.de>
2017-01-26 13:42:29 +0100

    implemented generative model call

--- DialogSystem/src/main/java/roboy/dialog/personality/states/AbstractBooleanState.java
+++ DialogSystem/src/main/java/roboy/dialog/personality/states/AbstractBooleanState.java
@@ -1,8 +1,14 @@
 package roboy.dialog.personality.states;
 
+import main.java.roboy.io.Ros;
+import main.java.roboy.io.Service;
+import main.java.roboy.io.ServiceRequest;
 import roboy.linguistics.Linguistics;
 import roboy.linguistics.sentenceanalysis.Interpretation;
 import roboy.util.Lists;
+import edu.wpi.rail.jrosbridge.Ros;
+import edu.wpi.rail.jrosbridge.Service;
+import edu.wpi.rail.jrosbridge.services.ServiceRequest;
 
 public abstract class AbstractBooleanState implements State{
 
@@ -36,6 +42,14 @@ public abstract class AbstractBooleanState implements State{
 	abstract protected boolean determineSuccess(Interpretation input);
 	
 	protected String callGenerativeModel(String sentence){
+		
+		Ros ros = new Ros("localhost");
+		ros.connect();
+		Service GenerativeModel = new Service(ros, "/roboy/gnlp_predict", "seq2seq_predict");
+	    ServiceRequest request = new ServiceRequest("{\"text_input\": " + "\"" + sentence + "\"}");
+	    String response = GenerativeModel.callServiceAndWait(request);
+	    System.out.println(response.ToString());
 		return "Generated text";
+		ros.disconnect();
 	}
 }
