d227133fef11bd7f6ad00a658b8d58703967ef38
Philipp Blohm <philippblohm@gmail.com>
2017-03-09 20:50:45 +0100

    Started PAS extraction on OpenNLP parse.

--- DialogSystem/src/main/java/roboy/linguistics/Linguistics.java
+++ DialogSystem/src/main/java/roboy/linguistics/Linguistics.java
@@ -1,5 +1,8 @@
 package roboy.linguistics;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Linguistics {
 
 	public enum SENTENCE_TYPE { 
@@ -9,6 +12,13 @@ public class Linguistics {
 		ANECDOTE,
 		WHO, HOW_IS, HOW_DO, WHY, WHEN, WHERE, WHAT, IS_IT, DOES_IT, STATEMENT, NONE
 		}
+	
+	public enum SEMANTIC_ROLE {
+		PREDICATE, AGENT, PATIENT, TIME, LOCATION, MANNER, INSTRUMENT, ORIGIN, DESTINATION,
+		RECIPIENT, BENEFICIARY, PURPOSE, CAUSE
+	}
+	
+	public static final List<String> tobe = Arrays.asList("am","are","is","was","were","been");
 	
 	public static final String SENTENCE = "sentence";
 	public static final String TRIPLE = "triple";
--- DialogSystem/src/main/java/roboy/linguistics/sentenceanalysis/OpenNLPParser.java
+++ DialogSystem/src/main/java/roboy/linguistics/sentenceanalysis/OpenNLPParser.java
@@ -3,6 +3,8 @@ package roboy.linguistics.sentenceanalysis;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 import opennlp.tools.cmdline.parser.ParserTool;
 import opennlp.tools.parser.Parse;
@@ -10,6 +12,7 @@ import opennlp.tools.parser.Parser;
 import opennlp.tools.parser.ParserFactory;
 import opennlp.tools.parser.ParserModel;
 import roboy.linguistics.Linguistics;
+import roboy.linguistics.Linguistics.SEMANTIC_ROLE;
 
 public class OpenNLPParser implements Analyzer{
 	
@@ -46,8 +49,74 @@ public class OpenNLPParser implements Analyzer{
 	
 	private Interpretation extractPAS(Interpretation interpretation, Parse parse){
 		System.out.println(parseToString(parse,0));
-		
+		System.out.println(pas(parse));
 		return interpretation;
+	}
+	
+	private Map<SEMANTIC_ROLE,Object> pas(Parse parse){
+		Map<SEMANTIC_ROLE,Object> result = new HashMap<>();
+		Parse[] children = parse.getChildren();
+		boolean modverb = false;
+		Object subject = null;
+		Object object = null;
+		String verb = null;
+		for(int i=0; i<children.length; i++){
+			if("NP".equals(children[i].getType())){
+				subject = children[i].getText();
+			} else if("VP".equals(children[i].getType())){
+				Parse[] vpChildren = children[i].getChildren();
+				for(int j=0; j<vpChildren.length; j++){
+					if("NP".equals(vpChildren[j].getType())){
+						object = vpChildren[j].getText();
+					} else if("VP".equals(vpChildren[j].getType())){
+						modverb = true;
+						Parse[] vp2Children = children[j].getChildren();
+						for(int k=0; k<vpChildren.length; k++){
+							if("NP".equals(vp2Children[k].getType())){
+								object = vp2Children[k].getText();
+							} else if(vp2Children[k].getType().startsWith("V")){
+								result.put(SEMANTIC_ROLE.PREDICATE, vp2Children[k].getText());
+							} else if("SBAR".equals(vp2Children[k].getType())){
+								Parse[] sbarChildren = vp2Children[k].getChildren();
+								for(int l=0; l<sbarChildren.length; l++){
+									if("S".equals(sbarChildren[l].getType())){
+										result.put(SEMANTIC_ROLE.PATIENT, pas(sbarChildren[l]));
+									}
+								}
+							} else if("S".equals(vp2Children[k].getType())){
+								result.put(SEMANTIC_ROLE.PATIENT, pas(vp2Children[k]));
+							}
+						}
+					} else if(vpChildren[j].getType().startsWith("V")){
+						verb = vpChildren[j].getText();
+					} else if("SBAR".equals(vpChildren[j].getType())){
+						Parse[] sbarChildren = vpChildren[j].getChildren();
+						for(int l=0; l<sbarChildren.length; l++){
+							if("S".equals(sbarChildren[l].getType())){
+								result.put(SEMANTIC_ROLE.PATIENT, pas(sbarChildren[l]));
+							}
+						}
+					} else if("S".equals(vpChildren[j].getType())){
+						result.put(SEMANTIC_ROLE.PATIENT, pas(vpChildren[j]));
+					}
+				}
+			} else if("S".equals(children[i].getType())){
+				result = pas(children[i]); // TODO: multiple PAS per sentence
+			}
+		}
+		// check passive
+		if(modverb && Linguistics.tobe.contains(verb.toLowerCase())){
+			if(subject!=null) result.put(SEMANTIC_ROLE.PATIENT, subject);
+			if(object!=null) result.put(SEMANTIC_ROLE.AGENT, object);
+		} else {
+			if(subject!=null) result.put(SEMANTIC_ROLE.AGENT, subject);
+			if(object!=null) result.put(SEMANTIC_ROLE.PATIENT, object);
+		}
+		if(!result.containsKey(SEMANTIC_ROLE.PREDICATE) && verb!=null){
+			result.put(SEMANTIC_ROLE.PREDICATE, verb);
+		}
+		
+		return result;
 	}
 	
 	public StringBuilder parseToString(Parse parse, int offset){
@@ -64,7 +133,8 @@ public class OpenNLPParser implements Analyzer{
 	}
 	
 	public static void main(String[] args) {
-		Interpretation i = new Interpretation("The man saw the boy with the binoculars.");
+		Interpretation i = new Interpretation("The man is seen by the boy with the binoculars.");
+//		Interpretation i = new Interpretation("Bill said that Mike like to play the piano.");
 		new OpenNLPParser().analyze(i);
 	}
 
