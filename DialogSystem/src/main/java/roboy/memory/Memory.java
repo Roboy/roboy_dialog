4fbc2d5d0ed55c88744b258c368f86f1fce27b41
missxa <aliona.kharchenko@gmail.com>
2017-03-23 23:34:28 +0100

    implemented memory interface using templates

--- DialogSystem/src/main/java/roboy/memory/Memory.java
+++ DialogSystem/src/main/java/roboy/memory/Memory.java
@@ -4,9 +4,7 @@ import java.io.IOException;
 import roboy.util.Concept;
 import roboy.util.Relation;
 
-public interface Memory {
-	public boolean save(Concept object) throws InterruptedException, IOException;
-	public boolean retrieve(Concept object) throws InterruptedException, IOException;
-	public boolean save(Relation triple) throws InterruptedException, IOException;
-	public boolean retrieve(Relation triple) throws InterruptedException, IOException;
+public interface Memory<T> {
+	public boolean save(T object) throws InterruptedException, IOException;
+	public T retrieve(T object) throws InterruptedException, IOException;
 }
