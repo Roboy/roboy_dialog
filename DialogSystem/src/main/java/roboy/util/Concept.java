e50e6d23fb74345f8e5fdb3aa484b081eded605f
missxa <aliona.kharchenko@gmail.com>
2017-03-24 16:46:05 +0100

    added getters and setters for Concept class

--- DialogSystem/src/main/java/roboy/util/Concept.java
+++ DialogSystem/src/main/java/roboy/util/Concept.java
@@ -1,10 +1,27 @@
 package roboy.util;
 
 import java.util.Map;
+import java.util.HashMap;
 
 public class Concept {
 	
 	private Map<String, Object> attributes;
+
+	public Concept()
+	{
+		this.attributes = new HashMap<String, Object>();
+	}
+
+	public void addAttribute(String property, Object value)
+	{
+		this.attributes.put(property, value);	
+	}
+
+	public void addAttributes(Map<String, Object> attrs)
+	{
+		this.attributes.putAll(attrs);
+	}
+
 	public Map<String, Object> getAttributes()
 	{
 		return this.attributes;
