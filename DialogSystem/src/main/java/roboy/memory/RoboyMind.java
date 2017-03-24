b72f20fcbd8a7f9fe0a104639d8d5b30bdad9e46
missxa <aliona.kharchenko@gmail.com>
2017-03-24 20:37:00 +0100

    minor changes in RoboyMind.java

--- DialogSystem/src/main/java/roboy/memory/RoboyMind.java
+++ DialogSystem/src/main/java/roboy/memory/RoboyMind.java
@@ -3,6 +3,10 @@ package roboy.memory;
 import java.util.List;
 import java.util.Map;
 
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonArray;
+
 import edu.wpi.rail.jrosbridge.Ros;
 import edu.wpi.rail.jrosbridge.Service;
 import edu.wpi.rail.jrosbridge.services.ServiceRequest;
@@ -11,7 +15,7 @@ import edu.wpi.rail.jrosbridge.services.ServiceResponse;
 import roboy.util.Concept;
 import roboy.util.Relation;
 
-public class RoboyMind implements Memory 
+public class RoboyMind implements Memory<Concept>
 {
 
 	private Ros ros;
@@ -48,7 +52,47 @@ public class RoboyMind implements Memory
 		return response;
 	}
 
-	private ServiceResponse ShowInstances(String object_class)
+	private ServiceResponse SaveObject(String object_class, int object_id, String properties, String values)
+	{
+		Service ShowInstanceSrv = new Service(ros, "/roboy_mind/save_object", "/roboy_mind/save_object");
+
+		JsonObject params = Json.createObjectBuilder()
+	     .add("object_class", "object_class")
+	     .add("id", object_id)
+	     .add("properties", properties)
+	     .add("values", values)
+	     .build();
+
+		// String params = "{\"object_class\": " + "\"" + object_class + "\", \"id\": " +  object_id + ", \"properties\": \"" +  properties + "\", \"values\": \"" +  values + "\"}";
+
+		System.out.println(params);
+
+		ServiceRequest request = new ServiceRequest(params);
+		ServiceResponse response = ShowInstanceSrv.callServiceAndWait(request);
+		return response;
+	}
+
+	// private Concept GetObject(String object_class, int object_id, String properties, String values)
+	// {
+	// 	Service ShowInstanceSrv = new Service(ros, "/roboy_mind/save_object", "/roboy_mind/save_object");
+
+	// 	JsonObject params = Json.createObjectBuilder()
+	//      .add("object_class", "object_class")
+	//      .add("id", object_id)
+	//      .add("properties", properties)
+	//      .add("values", values)
+	//      .build();
+
+	// 	// String params = "{\"object_class\": " + "\"" + object_class + "\", \"id\": " +  object_id + ", \"properties\": \"" +  properties + "\", \"values\": \"" +  values + "\"}";
+
+	// 	System.out.println(params);
+
+	// 	ServiceRequest request = new ServiceRequest(params);
+	// 	ServiceResponse response = ShowInstanceSrv.callServiceAndWait(request);
+	// 	return response;
+	// }
+
+	private ServiceResponse ShowInstance(String object_class)
 	{
 		Service ShowInstanceSrv = new Service(ros, "/roboy_mind/show_instances", "/roboy_mind/show_instances");
 		String params = "{\"object_class\": " + "\"" + object_class + "\"}";
@@ -57,50 +101,55 @@ public class RoboyMind implements Memory
 		return response;
 	}
 
+
 	@Override
 	public boolean save(Concept object) 
 	{
-		//TODO combine both services on the Python side
+
 		//create an object
 		String object_class = object.getAttributes().get("class").toString();
 		int object_id = (int) object.getAttributes().get("id");
-		CreateInstance(object_class, object_id);
-		String object_name = object_class + object_id;
 		
-		//assign attributes
+		String properties = ""; 
+		String values = "";
+
+		//separate values and attributes
 		for (Map.Entry<String, Object> attribute : object.getAttributes().entrySet())
 		{
 		    if (attribute.getKey() != "class" && attribute.getKey() != "id")
 		    {
-		    	String property = attribute.getKey();
-		    	Object instance = attribute.getValue();
-		    	AssertProperty(object_name,  property,  instance,  true);
+		    	// valuesList.add(attribute.getValue().toString());
+		    	// propertiesList.add(attribute.getKey());
+		    	properties += "\n- \'" + attribute.getKey() + "\'";
+		    	values += "\n- \'" + attribute.getValue().toString() + "\'";
 		    }
-			
 		}
+
+		SaveObject(object_class, object_id, properties, values);
+
 		//TODO check the service response
 		return true;
 	}
 	
 	@Override
-	public boolean retrieve(Concept object)
+	public Concept retrieve(Concept object)
 	{
 		// TODO implement service on the python side
-		return true;
+		return object;
 	}
 	
-	@Override
-	public boolean save(Relation object) 
-	{
-		// TODO define relations in Python roboy_mind
-		return false;
-	}
+	// @Override
+	// public boolean save(Relation object) 
+	// {
+	// 	// TODO define relations in Python roboy_mind
+	// 	return false;
+	// }
 	
-	@Override
-	public boolean retrieve(Relation object) 
-	{
-		// TODO define relations in Python roboy_mind
-		return false;
-	}
+	// @Override
+	// public boolean retrieve(Relation object) 
+	// {
+	// 	// TODO define relations in Python roboy_mind
+	// 	return false;
+	// }
 	
 }
