6bba5d7126bc6c59a9c7b3f8d1ffdd831d79d5d9
Petr <petr.2@mail.ru>
2017-04-02 23:39:13 +0400

    audioIO packages cleaned

--- DialogSystem/beads/src/beads_main/net/beadsproject/beads/data/audiofile/AudioFileType.java
+++
@@ -1,9 +0,0 @@
-package net.beadsproject.beads.data.audiofile;
-
-/**
- * Audio file types that may be supported by an AudioFileReader or AudioFileWriter
- * @author aengus
- */
-public enum AudioFileType {
-	WAV, AIFF, MP3, OGG, WMA;
-}
