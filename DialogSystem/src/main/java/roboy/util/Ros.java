6d439ab32bb51f4ce49e92f55b077addf43e86c6
missxa <aliona.kharchenko@gmail.com>
2017-04-02 02:11:40 +0200

    reversed ros.java changes

--- DialogSystem/src/main/java/roboy/util/Ros.java
+++ DialogSystem/src/main/java/roboy/util/Ros.java
@@ -4,18 +4,17 @@ public class Ros {
 
     private static edu.wpi.rail.jrosbridge.Ros ros;
 
-//    private static final String ROS_URL = "localhost";
+    private static final String ROS_URL = "localhost";
 
-    private Ros(String ros_url) {
-        ros = new edu.wpi.rail.jrosbridge.Ros(ros_url);
+    private Ros() {
+        ros = new edu.wpi.rail.jrosbridge.Ros();
         ros.connect();
         System.out.println("ROS bridge is set up");
     }
 
     public static edu.wpi.rail.jrosbridge.Ros getInstance(){
         if(ros == null){
-            System.out.println("ROS bridge has not been set up yet. Shutting down...");
-            System.exit(0);
+        	new Ros();
         }
         return ros;
     }
