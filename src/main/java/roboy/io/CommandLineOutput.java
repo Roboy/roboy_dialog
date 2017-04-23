5c1c92feee452fdee096db2ff2bfbb467dd95333
missxa <aliona.kharchenko@gmail.com>
2017-04-24 00:31:42 +0200

    removed emotion output from cmd line

--- src/main/java/roboy/io/CommandLineOutput.java
+++ src/main/java/roboy/io/CommandLineOutput.java
@@ -11,17 +11,17 @@ import roboy.dialog.action.SpeechAction;
  */
 public class CommandLineOutput implements OutputDevice{
 
-	private EmotionOutput emotion;
-	public CommandLineOutput(EmotionOutput emotion)
-	{
-		this.emotion = emotion;
-	}
+//	private EmotionOutput emotion;
+//	public CommandLineOutput(EmotionOutput emotion)
+//	{
+//		this.emotion = emotion;
+//	}
 	@Override
 	public void act(List<Action> actions) {
 		for(Action a : actions){
 			if(a instanceof SpeechAction){
-				int duration = ((SpeechAction) a).getText().length()==0 ? 0 : 1 + ((SpeechAction) a).getText().length()/8;
-				emotion.act(new FaceAction("speak", duration));
+//				int duration = ((SpeechAction) a).getText().length()==0 ? 0 : 1 + ((SpeechAction) a).getText().length()/8;
+//				emotion.act(new FaceAction("speak", duration));
 				System.out.println(((SpeechAction) a).getText());
 			}
 		}
