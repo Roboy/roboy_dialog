f0cccb544521773a0370a9adaee04eef04850c46
Laura Lahesoo <laura.lahesoo@tum.de>
2017-06-22 20:31:18 +0200

    Commented out DKPro dependencies, documented the current state of the DKPro example.

--- src/main/java/roboy/linguistics/sentenceanalysis/dkpro_example/PipelineExample.java
+++ src/main/java/roboy/linguistics/sentenceanalysis/dkpro_example/PipelineExample.java
@@ -15,13 +15,15 @@ import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpSegmenter;
  * This is a linguistic analysis pipeline example from:
  * https://dkpro.github.io/dkpro-core/pages/java-intro/
  *
- * If you wish to test the pipeline, write your input into the file document.txt
- * The output will be written in document.txt.conll
+ * If you wish to test the pipeline:
+ * 1) uncomment the DKPro dependencies in pom.xml
+ * 2) write your input into the file document.txt
+ * 3) The output will be written in document.txt.conll
  *
- * If you wish to continue integrating SRL tools, you may start where I left off by
- * uncommenting the semantic role labeler dependencies (and hashMap due to some
- * interdependency) here and in the pom file. At the time of writing, neither of
- * the labelers worked: ClearNlp SRL does not produce output and MateSRL throws a
+ * If you wish to continue integrating SRL tools, you should start by
+ * uncommenting the additional dependencies in pom and under imports.
+ * At the time of writing, neither of the labelers worked:
+ * ClearNlp SRL does not produce output, and MateSRL throws a
  * ClassNotFoundException - possibly due to some dependency issues.
  *
  */
