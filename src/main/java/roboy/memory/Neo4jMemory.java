79047aaf26f1180c36653a2ef146d60e05fde55e
Laura Lahesoo <laura.lahesoo@tum.de>
2017-07-20 16:49:11 +0200

    Added connection to Neo4j memory.

---
+++ src/main/java/roboy/memory/Neo4jMemory.java
@@ -0,0 +1,29 @@
+package roboy.memory;
+
+import roboy.util.RosMainNode;
+
+import java.io.IOException;
+import java.util.*;
+
+public class Neo4jMemory implements Memory<String>
+{
+    private RosMainNode rosMainNode;
+
+    public Neo4jMemory (RosMainNode node){
+        this.rosMainNode = node;
+    }
+
+    @Override
+    public boolean save(String query) throws InterruptedException, IOException
+    {
+        return rosMainNode.SendToMemory(query);
+    }
+
+    @Override
+    public List<String> retrieve(String query) throws InterruptedException, IOException
+    {
+        // TODO return the result object - needs to be implemented first
+        boolean res = rosMainNode.QueryMemory(query);
+        return new ArrayList<String>();
+    }
+}
