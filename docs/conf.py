a6bbfb8b3c564343313b1b11a630ec3c5ce5628b
Alona Kharchenko <aliona.kharchenko@gmail.com>
2017-09-01 20:04:05 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -360,7 +360,7 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/' :None}
+intersphinx_mapping = {'https://docs.python.org/2.7/' :None}
 
 import sphinx.environment
 from docutils.utils import get_source_line
