030ca216d29cff9db3d07efd2a1f9f6ef739da63
Laura Lahesoo <laura.lahesoo@tum.de>
2018-01-18 23:49:55 +0100

    GUI done, need to add update functionality.

--- src/main/java/roboy/context/GUI/ContextGUI.java
+++ src/main/java/roboy/context/GUI/ContextGUI.java
@@ -1,13 +1,16 @@
 package roboy.context.GUI;
 
+import org.apache.commons.lang3.tuple.Pair;
 import roboy.context.Context;
 import roboy.context.ValueAttribute;
 
 import javax.swing.*;
 import javax.swing.border.LineBorder;
+import javax.swing.border.TitledBorder;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,14 +21,14 @@ import java.util.Map;
 public class ContextGUI {
     private JFrame mainFrame;
 
-    String[][] historyAttributeData;
-
-    private JLabel headerLabel = new JLabel("test1",JLabel.CENTER);
-    private JLabel statusLabel = new JLabel("test2",JLabel.CENTER);
     private JPanel controlPanel;
-    private JLabel valueHeader;
+    private TitledBorder valueBorder;
     private JPanel valuePanel;
-    private JLabel msgLabel = new JLabel("Welcome to TutorialsPoint SWING Tutorial.", JLabel.CENTER);
+    private TitledBorder historyBorder;
+    private JPanel historyPanel;
+
+    private static int FULL_WIDTH = 400;
+    private static int FULL_HEIGHT = 400;
 
     public static void main(String[] args) {
         ContextGUI gui = new ContextGUI();
@@ -39,16 +42,21 @@ public class ContextGUI {
     private void prepareGUI() {
         // Window initialization.
         mainFrame = new JFrame("Context GUI (alpha)");
-        mainFrame.setSize(400, 200);
-        mainFrame.setLayout(new BoxLayout(mainFrame.getContentPane(), BoxLayout.Y_AXIS));
+        mainFrame.setSize(FULL_WIDTH, FULL_HEIGHT);
+        mainFrame.setLayout(new FlowLayout());
+        JPanel mainPanel = new JPanel();
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(2,2,2,2));
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.PAGE_AXIS));
+
+        mainFrame.add(mainPanel);
 
         // Attribute part initialization.
-        valueHeader = new JLabel("Context values", JLabel.CENTER);
-        valueHeader.setBorder(new LineBorder(Color.BLUE));
-        mainFrame.add(valueHeader);
-
         valuePanel = new JPanel();
         valuePanel.setLayout(new GridLayout(0,2));
+        valueBorder = BorderFactory.createTitledBorder("Context values");
+        valueBorder.setTitleJustification(TitledBorder.CENTER);
+        valuePanel.setBorder(valueBorder);
+
         Map<Context.ValueAttributes, JLabel[]> values = new HashMap<>();
         for(Context.ValueAttributes v : Context.ValueAttributes.values()) {
             Object val = v.getLastValue();
@@ -56,15 +64,37 @@ public class ContextGUI {
                 val = "<not initialized>";
             }
             JLabel[] pair = {
-                    new JLabel(v.toString(), JLabel.CENTER),
+                    new JLabel(v.toString()+ ":", JLabel.CENTER),
                     new JLabel(val.toString(),JLabel.CENTER)};
             values.put(v, pair);
             valuePanel.add(pair[0]);
             valuePanel.add(pair[1]);
-
         }
+        mainPanel.add(valuePanel);
 
-        mainFrame.add(valuePanel);
+        historyPanel = new JPanel();
+        historyPanel.setLayout(new GridLayout(0,2));
+        historyBorder = BorderFactory.createTitledBorder("Histories");
+        historyBorder.setTitleJustification(TitledBorder.CENTER);
+        historyPanel.setBorder(historyBorder);
+
+        Map<Context.HistoryAttributes, JList> history = new HashMap<>();
+        for(Context.HistoryAttributes v : Context.HistoryAttributes.values()) {
+            Object[] vals = v.getNLastValues(Integer.MAX_VALUE).entrySet().toArray();
+            if (vals.length == 0) {
+                vals = new Object[]{"<not initialized>", "<not initialized>", "<not initialized>", "<not initialized>", "<not initialized>", "<not initialized>",
+                        "<not initialized>", "<not initialized>", "<not initialized>", "<not initialized>", "<not initialized>", "<not initialized>",
+                        "<not initialized>", "<not initialized>", "<not initialized>", "<not initialized>", "<not initialized>", "<not initialized>",
+                        "<not initialized>"};
+            }
+            JList historyList = new JList(vals);
+
+            JScrollPane scrollPane = new JScrollPane();
+            scrollPane.setViewportView(historyList);
+            historyPanel.add(new JLabel(v.toString() + ":", JLabel.CENTER));
+            historyPanel.add(scrollPane);
+        }
+        mainPanel.add(historyPanel);
 
         mainFrame.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent windowEvent){
@@ -74,35 +104,20 @@ public class ContextGUI {
 
         controlPanel = new JPanel();
         controlPanel.setLayout(new FlowLayout());
-        statusLabel.setSize(100,100);
 
-        mainFrame.add(headerLabel);
-        mainFrame.add(controlPanel);
-        mainFrame.add(statusLabel);
-        mainFrame.setVisible(true);
+        mainPanel.add(controlPanel);
+        mainPanel.setVisible(true);
     }
 
     private void showJFrameDemo() {
-        headerLabel.setText("Container in action: JFrame");
-        final JFrame frame = new JFrame();
-        frame.setSize(300, 300);
-        frame.setLayout(new FlowLayout());
-        frame.add(msgLabel);
-
-
-        frame.addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent windowEvent){
-                frame.dispose();
-            }
-        });
-        JButton okButton = new JButton("Open a Frame");
-        okButton.addActionListener(e -> {
-                headerLabel.setText("Changing text");
-                statusLabel.setText("A Frame shown to the user.");
-                frame.setVisible(true);
+        JButton updateButton = new JButton("Update");
+        updateButton.addActionListener(e -> {
+                valueBorder.setTitle("Updated in the future");
+                valuePanel.setBorder(valueBorder);
+                valuePanel.updateUI();
             }
         );
-        controlPanel.add(okButton);
+        controlPanel.add(updateButton);
         mainFrame.setVisible(true);
     }
 }
