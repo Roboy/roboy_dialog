dd154ebb73684d2968ff51fcc7057a1739481ee0
Laura Lahesoo <laura.lahesoo@tum.de>
2018-02-23 22:41:11 +0100

    Code review changes.

--- src/integration-test/java/roboy/context/ContextIntegrationTest.java
+++ src/integration-test/java/roboy/context/ContextIntegrationTest.java
@@ -17,14 +17,14 @@ public class ContextIntegrationTest {
      * For this test to work, start the TEST_TOPIC subscriber defined under RosSubscribers.
      */
     @Test
-    public void getLastAttributeValue() throws Exception {
+    public void checkROSTopicUpdating() throws Exception {
         int updateFrequency = 1; //Assuming the updater's frequency is 1 second!
         int sleeptime = updateFrequency * 1000 * 2; // Here in millis and double the actual update time.
         for(int i = 0; i < 3; i++) {
-            int values = Context.ROS_TEST.valuesAddedSinceStart();
+            int values = Context.getInstance().ROS_TEST.valuesAddedSinceStart();
             Thread.sleep(sleeptime);
             assertNotEquals("New test values should have been added during idle time!",
-                    Context.ROS_TEST.valuesAddedSinceStart(), values);
+                    Context.getInstance().ROS_TEST.valuesAddedSinceStart(), values);
         }
     }
 
