8baaf4f4a911a247eff639b7afbadf2d19fffa4b
Wagram Airiian <vagram.airiian@tum.de>
2018-03-06 23:12:55 +0100

    Refactor Json intent QA parser

---
+++ src/main/java/roboy/util/QAJsonParser.java
@@ -0,0 +1,119 @@
+package roboy.util;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonSyntaxException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import roboy.memory.Neo4jRelationships;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Map;
+
+class JsonModel {
+    // TODO: Dynamically create JsonModel as JsonEntryModel fields based on existing Neo4jRelationships entries
+    // Add a new entry
+    JsonEntryModel name;
+    JsonEntryModel FROM;
+    JsonEntryModel HAS_HOBBY;
+    JsonEntryModel LIVE_IN;
+    JsonEntryModel FRIEND_OF;
+    JsonEntryModel STUDY_AT;
+    JsonEntryModel MEMBER_OF;
+    JsonEntryModel WORK_FOR;
+    JsonEntryModel OCCUPIED_AS;
+    JsonEntryModel IS;
+}
+
+/**
+ * Getting values for personal and follow-up questions
+ * from a JSON file
+ */
+public class QAJsonParser {
+    private Gson gson;
+    private JsonModel jsonObject;
+
+    final Logger LOGGER = LogManager.getLogger();
+
+    public QAJsonParser() {
+        jsonObject = null;
+    }
+
+    public QAJsonParser(String file) {
+        jsonObject = parse(file);
+    }
+
+    public JsonModel parse(String file) {
+        try {
+            InputStream input = JsonUtils.class.getClassLoader().getResourceAsStream(file);
+            BufferedReader br = new BufferedReader(new InputStreamReader(input));
+            gson = new Gson();
+            return gson.fromJson(br, JsonModel.class);
+        } catch (JsonSyntaxException e) {
+            LOGGER.error("Wrong syntax in QA json file" + e.getMessage());
+        } catch (JsonIOException e) {
+            LOGGER.error("IO Error on parsing QA json file from BufferedReader: " + e.getMessage());
+        } catch (JsonParseException e) {
+            LOGGER.error("Parsing error on processing QA json file: " + e.getMessage());
+        } catch (Exception e) {
+            LOGGER.error("Unexpected error while parsing QA values from json: " + e.getMessage());
+        }
+        return null;
+    }
+
+    public JsonModel getQA() {
+        return jsonObject;
+    }
+
+    public JsonEntryModel getEntry(Neo4jRelationships relationship) {
+        JsonEntryModel entryValue = null;
+        if (jsonObject != null) {
+            try {
+                Class<JsonEntryModel> types = JsonEntryModel.class;
+                Field field = jsonObject.getClass().getDeclaredField(relationship.type);
+                field.setAccessible(true);
+                Object value = field.get(jsonObject);
+                entryValue = (JsonEntryModel) value;
+            } catch (NoSuchFieldException e) {
+                LOGGER.error("No such entry in QA model: " + e.getMessage());
+            } catch (IllegalAccessException e) {
+                LOGGER.error("Illegal access to the QA entries: " + e.getMessage());
+            }
+        }
+
+        return entryValue;
+    }
+
+    public List<String> getQuestions(Neo4jRelationships relationship) {
+        return getEntry(relationship).getQuestions();
+    }
+
+    public Map<String, List<String>> getAnswers(Neo4jRelationships relationship) {
+        return getEntry(relationship).getAnswers();
+    }
+
+    public List<String> getSuccessAnswers(Neo4jRelationships relationship) {
+        return getAnswers(relationship).get("SUCCESS");
+    }
+
+    public List<String> getFailureAnswers(Neo4jRelationships relationship) {
+        return getAnswers(relationship).get("FAILURE");
+    }
+
+    public Map<String, List<String>> getFollowUp(Neo4jRelationships relationship) {
+        return getEntry(relationship).getFUP();
+    }
+
+    public List<String> getFollowUpQuestions(Neo4jRelationships relationship) {
+        return getFollowUp(relationship).get("Q");
+    }
+
+    public List<String> getFollowUpAnswers(Neo4jRelationships relationship) {
+        return getFollowUp(relationship).get("A");
+    }
+}
