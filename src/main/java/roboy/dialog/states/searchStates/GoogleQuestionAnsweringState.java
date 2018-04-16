b031ed18016fa455d12680122293a0ed5ca5b73b
Wagram Airiian <vagram.airiian@tum.de>
2018-04-16 14:01:50 +0200

    Added class stubs for the small personalities

---
+++ src/main/java/roboy/dialog/states/searchStates/GoogleQuestionAnsweringState.java
@@ -0,0 +1,35 @@
+package roboy.dialog.states.searchStates;
+
+import roboy.dialog.states.definitions.State;
+import roboy.dialog.states.definitions.StateParameters;
+import roboy.linguistics.sentenceanalysis.Interpretation;
+
+public class GoogleQuestionAnsweringState extends State {
+    /**
+     * Create a state object with given identifier (state name) and parameters.
+     * <p>
+     * The parameters should contain a reference to a state machine for later use.
+     * The state will not automatically add itself to the state machine.
+     *
+     * @param stateIdentifier identifier (name) of this state
+     * @param params          parameters for this state, should contain a reference to a state machine
+     */
+    public GoogleQuestionAnsweringState(String stateIdentifier, StateParameters params) {
+        super(stateIdentifier, params);
+    }
+
+    @Override
+    public Output act() {
+        return null;
+    }
+
+    @Override
+    public Output react(Interpretation input) {
+        return null;
+    }
+
+    @Override
+    public State getNextState() {
+        return null;
+    }
+}
