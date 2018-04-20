eda19c7bc47f3f02b795bf1b4076270307d8402e
Wagram Airiian <vagram.airiian@tum.de>
2018-04-20 17:18:15 +0200

    Add the input sentiment check in the RoboyInfoState

--- src/main/java/roboy/dialog/states/expoStates/RoboyInfoState.java
+++ src/main/java/roboy/dialog/states/expoStates/RoboyInfoState.java
@@ -1,10 +1,21 @@
 package roboy.dialog.states.expoStates;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import roboy.dialog.states.definitions.State;
 import roboy.dialog.states.definitions.StateParameters;
+import roboy.linguistics.Linguistics.UtteranceSentiment;
 import roboy.linguistics.sentenceanalysis.Interpretation;
 
+
 public class RoboyInfoState extends State {
+    private final String SELECTED_SKILLS = "skills";
+    private final String SELECTED_ABILITIES = "abilities";
+    private final String SELECTED_ROBOY_QA = "roboy";
+    private final String LEARN_ABOUT_PERSON = "newPerson";
+    private final Logger LOGGER = LogManager.getLogger();
+
+    private State nextState;
 
     public RoboyInfoState(String stateIdentifier, StateParameters params) {
         super(stateIdentifier, params);
@@ -12,16 +23,23 @@ public class RoboyInfoState extends State {
 
     @Override
     public Output act() {
-        return null;
+        return Output.say("");
     }
 
     @Override
     public Output react(Interpretation input) {
-        return null;
+        UtteranceSentiment inputSentiment = getInference().inferSentiment(input);
+        if (inputSentiment.toBoolean == Boolean.TRUE) {
+            nextState = null;
+            return Output.say("");
+        } else {
+            nextState = getTransition(LEARN_ABOUT_PERSON);
+            return Output.say("");
+        }
     }
 
     @Override
     public State getNextState() {
-        return null;
+        return nextState;
     }
 }
