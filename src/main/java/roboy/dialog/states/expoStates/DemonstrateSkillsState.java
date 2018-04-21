6856b8c284e0ba05db8edb03f07f5a16577e4ae6
Wagram Airiian <vagram.airiian@tum.de>
2018-04-21 21:44:28 +0200

    Refactored the DemonstrateSkillsState

--- src/main/java/roboy/dialog/states/expoStates/DemonstrateSkillsState.java
+++ src/main/java/roboy/dialog/states/expoStates/DemonstrateSkillsState.java
@@ -29,8 +29,13 @@ public class DemonstrateSkillsState extends State {
 
     private final Logger LOGGER = LogManager.getLogger();
 
-    private final RandomList<String> offerPhrases = new RandomList<>("Do you want me to %s?", "Would you like me to %s?", "Should I demonstrate you how I %s?");
-    private final RandomList<String> connectingPhrases = PhraseCollection.SEGUE_CONNECTING_PHRASES;
+    private final RandomList<String> offerJokes = PhraseCollection.OFFER_JOKES_PHRASES;
+    private final RandomList<String> offerFacts = PhraseCollection.OFFER_FACTS_PHRASES;
+    private final RandomList<String> offerFamousEntities = PhraseCollection.OFFER_FAMOUS_ENTITIES_PHRASES;
+    private final RandomList<String> offerMath = PhraseCollection.OFFER_MATH_PHRASES;
+    private final RandomList<String> jokes = PhraseCollection.JOKES;
+    private final RandomList<String> facts = PhraseCollection.FACTS;
+    private final RandomList<String> connectingPhrases = PhraseCollection.CONNECTING_PHRASES;
     // private final RandomList<String> positivePhrases = new RandomList<>(" lets do it!", " behold me!", " I will give you the show of your life!", " that is the best decision!");
     private final RandomList<String> negativePhrases = new RandomList<>(" let me ask you a question then!", " was I not polite enough?", " lets switch gears!", " lets change the topic!");
     private final RandomList<String> roboyQAPhrases = new RandomList<>(" answer some questions about myself", " tell you more about myself");
@@ -46,11 +51,33 @@ public class DemonstrateSkillsState extends State {
     public Output act() {
         IntentValue intentValue = Context.getInstance().DIALOG_INTENTS.getLastValue();
         if (intentValue.getNeo4jPropertyValue() == Neo4jProperty.skills) {
+            LOGGER.info("Extracted intent value: [" +
+                    intentValue.getId() + " / " +
+                    intentValue.getNeo4jPropertyValue() + " / " +
+                    intentValue.getAttribute() + "]");
             currentSubintent = detectSubintent(intentValue.getAttribute());
-            return Output.say(getOfferSentence(intentValue.getAttribute()));
+            if (currentSubintent != null) {
+                LOGGER.info("Extracted the following subintent: " + currentSubintent);
+                switch (currentSubintent) {
+                    case jokes:
+                        return Output.say(offerJokes.getRandomElement());
+                    case math:
+                        return Output.say(offerMath.getRandomElement());
+                    case fun_facts:
+                        return Output.say(offerFacts.getRandomElement());
+                    case famous_entities:
+                        return Output.say(offerFamousEntities.getRandomElement());
+                }
+            } else {
+                LOGGER.info("Subintent extraction failed");
+            }
         } else {
-            return Output.useFallback();
+            LOGGER.error("Unexpected intent value: [" +
+                    intentValue.getId() + " / " +
+                    intentValue.getNeo4jPropertyValue() + " / " +
+                    intentValue.getAttribute() + "]");
         }
+        return Output.sayNothing();
     }
 
     @Override
@@ -61,6 +88,20 @@ public class DemonstrateSkillsState extends State {
         LOGGER.info("The detected sentiment is " + inputSentiment);
         if (inputSentiment.toBoolean == Boolean.TRUE) {
             String answer = "";
+            if (currentSubintent != null) {
+                switch (currentSubintent) {
+                    case jokes:
+                        answer = getRandomJoke();
+                    case fun_facts:
+                        answer = getRandomFact();
+                    case famous_entities:
+                        // Requires SEM_PARSER
+                        break;
+                    case math:
+                        // Requires SEM_PARSER
+                        break;
+                }
+            }
             nextState = getRandomTransition();
             return Output.say(getConnectingPhrase(person.getName()) + answer);
         } else {
@@ -72,10 +113,6 @@ public class DemonstrateSkillsState extends State {
     @Override
     public State getNextState() {
         return nextState;
-    }
-
-    private String getOfferSentence(String roboyOfferPhrase) {
-        return String.format(offerPhrases.getRandomElement(), roboyOfferPhrase);
     }
 
     private String getConnectingPhrase(String name) {
@@ -107,5 +144,13 @@ public class DemonstrateSkillsState extends State {
             default:
                 return getTransition(LEARN_ABOUT_PERSON);
         }
+    }
+
+    private String getRandomJoke() {
+        return jokes.getRandomElement();
+    }
+
+    private String getRandomFact() {
+        return facts.getRandomElement();
     }
 }
