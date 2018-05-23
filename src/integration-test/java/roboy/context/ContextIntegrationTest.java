bfa994a82ea915ff42e1e765c2be919e5115f7dd
mireu <rackwitz.frederic@gmail.com>
2018-05-23 13:53:33 +0200

    Disabled disfunctional test.

--- src/integration-test/java/roboy/context/ContextIntegrationTest.java
+++ src/integration-test/java/roboy/context/ContextIntegrationTest.java
@@ -1,20 +1,23 @@
 package roboy.context;
 
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import roboy.ros.RosMainNode;
 
 import static org.junit.Assert.assertNotEquals;
 
+@Ignore
 public class ContextIntegrationTest {
-    @BeforeClass
 
     /**
      * For this test to work, start the TEST_TOPIC subscriber defined under RosSubscribers.
      */
     @Test
     public void checkROSTopicUpdating() throws Exception {
+        RosMainNode ros = new RosMainNode();
         Context context = new Context();
+        context.initializeROS(ros);
 
         int updateFrequency = 1; //Assuming the updater's frequency is 1 second!
         int sleeptime = updateFrequency * 1000 * 2; // Here in millis and double the actual update time.
