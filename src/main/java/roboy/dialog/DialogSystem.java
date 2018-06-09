f308a603c0cdf92085f568c73767c3a5010a0105
Wagram Airiian <vagram.airiian@tum.de>
2018-06-09 17:49:47 +0200

    Input object refactoring

--- src/main/java/roboy/dialog/DialogSystem.java
+++ src/main/java/roboy/dialog/DialogSystem.java
@@ -126,7 +126,7 @@ public class DialogSystem {
                 }
 
                 // analyze
-                Interpretation interpretation = new Interpretation(raw.sentence, raw.attributes);
+                Interpretation interpretation = new Interpretation(raw.getSentence(), raw.getAttributes());
                 for (Analyzer a : analyzers) {
                     try {
                         interpretation = a.analyze(interpretation);
