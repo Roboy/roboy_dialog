8721804d7583bbfe2c4e359eaf5b4432246392f2
Jayce <luoyizhong98+git@gmail.com>
2018-06-21 17:01:46 +0200

    [Deprecation] deprecated ContextIntegrationTest
    No longer needed due to removal of ROS

--- dialog/src/integration-test/java/roboy/context/ContextIntegrationTest.java
+++ dialog/src/integration-test/java/roboy/context/ContextIntegrationTest.java
@@ -2,10 +2,13 @@ package roboy.context;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.Ignore;
 import roboy.ros.RosMainNode;
 
 import static org.junit.Assert.assertNotEquals;
 
+
+@Deprecated
 public class ContextIntegrationTest {
     @BeforeClass
     public static void initializeWithROS() {
@@ -16,6 +19,7 @@ public class ContextIntegrationTest {
     /**
      * For this test to work, start the TEST_TOPIC subscriber defined under RosSubscribers.
      */
+    @Ignore
     @Test
     public void checkROSTopicUpdating() throws Exception {
         int updateFrequency = 1; //Assuming the updater's frequency is 1 second!
