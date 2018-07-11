1785bc1a2d9a5b4c9e900829aa77ff65077a5fb6
Jayce <luoyizhong98+git@gmail.com>
2018-07-11 22:20:43 +0200

    Deprecation of SimpleTokenizer

--- dialog/src/main/java/roboy/linguistics/sentenceanalysis/SimpleTokenizer.java
+++ dialog/src/main/java/roboy/linguistics/sentenceanalysis/SimpleTokenizer.java
@@ -8,7 +8,27 @@ import java.util.List;
 /**
  * Tokenizes the text by splitting at whitespace and stores the resulting tokens in the
  * Linguistics.TOKENS attribute of the interpretation.
+ *
+ * This class was deprecated because the tokens are anyways overwritten by SemanticParserAnalyzer. SmeanticParserAnalyzer is run next and does not require the analysis of SimpleTokenizer to function. Furthermore ST's approach to Parsing is not exactly correct.
+ *
+ * [Roboy]: Do I need a visa to go to suzhou?
+ * [You]:   Yes Roboy, you do
+ *
+ * Simple Tokenizer:
+ *     0 = "yes"
+ *     1 = "roboy,"
+ *     2 = "you"
+ *     3 = "do"
+ *
+ * Semantic Parser Analyzer: (Desired)
+ *     0 = "yes"
+ *     1 = "roboy"
+ *     2 = ","
+ *     3 = "you"
+ *     4 = "do"
+ *
  */
+@Deprecated
 public class SimpleTokenizer implements Analyzer{
 
 	@Override
