85c2a930e0b12e4e171e0291f14cb25c4c8b01c2
Cagri <cagri.yildiz@tum.de>
2018-08-16 13:52:33 +0200

    pic command fixes

--- dialog/src/main/java/roboy/util/TelegramCommandHandler.java
+++ dialog/src/main/java/roboy/util/TelegramCommandHandler.java
@@ -87,8 +87,8 @@ public class TelegramCommandHandler
         {
             case PIC:
                 //Just a roboy image, should be changed to desired roboy url
-                int randomNum = ThreadLocalRandom.current().nextInt(0, pictureUrls.length+ 1);
-                String url = pictureUrls[randomNum-1];
+                int randomNum = ThreadLocalRandom.current().nextInt(0, pictureUrls.length);
+                String url = pictureUrls[randomNum];
                 tch.sendImageFromUrl(url, telegramChatID);
                 break;
             case SAY:
