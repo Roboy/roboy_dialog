d0d11aa9e5e2c7d7aa40daa8c432b843bb6d31bd
Wagram Airiian <vagram.airiian@tum.de>
2018-09-16 16:27:44 +0200

    Updated QAFileParser and incorporated into QAJsonParser + backward compatibility

--- dialog/src/main/java/roboy/util/JsonEntryModel.java
+++ dialog/src/main/java/roboy/util/JsonEntryModel.java
@@ -1,11 +1,33 @@
 package roboy.util;
 
 import java.util.Map;
+import java.util.Objects;
 
 public class JsonEntryModel {
+    String intent;
     RandomList<String> Q;
     Map<String, RandomList<String>> A;
     Map<String, RandomList<String>> FUP;
+
+    public void setIntent(String intent) {
+        this.intent = intent;
+    }
+
+    public void setQuestions(RandomList<String> q) {
+        Q = q;
+    }
+
+    public void setAnswers(Map<String, RandomList<String>> a) {
+        A = a;
+    }
+
+    public void setFUP(Map<String, RandomList<String>> FUP) {
+        this.FUP = FUP;
+    }
+
+    public String getIntent() {
+        return intent;
+    }
 
     public RandomList<String> getQuestions() {
         return Q;
@@ -17,5 +39,32 @@ public class JsonEntryModel {
 
     public Map<String, RandomList<String>> getFUP() {
         return FUP;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof JsonEntryModel)) return false;
+        JsonEntryModel that = (JsonEntryModel) o;
+        return Objects.equals(getIntent(), that.getIntent()) &&
+                Objects.equals(Q, that.Q) &&
+                Objects.equals(A, that.A) &&
+                Objects.equals(getFUP(), that.getFUP());
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(getIntent(), Q, A, getFUP());
+    }
+
+    @Override
+    public String toString() {
+        return "JsonEntryModel{" +
+                "intent='" + intent + '\'' +
+                ", Q=" + Q +
+                ", A=" + A +
+                ", FUP=" + FUP +
+                '}';
     }
 }
