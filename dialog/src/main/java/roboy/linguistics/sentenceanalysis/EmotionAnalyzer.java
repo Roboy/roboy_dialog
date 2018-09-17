eca077a6856e4291fa0da4b503b7d0e8fca45fd0
mireu <rackwitz.frederic@gmail.com>
2018-09-17 22:12:56 +0200

    Removed old emotionanalyzer comment

--- dialog/src/main/java/roboy/linguistics/sentenceanalysis/EmotionAnalyzer.java
+++ dialog/src/main/java/roboy/linguistics/sentenceanalysis/EmotionAnalyzer.java
@@ -133,29 +133,6 @@ public class EmotionAnalyzer implements Analyzer {
         }
 
         return interpretation;
-
-//        // OLD EMOTION ANALYZER
-//        List<String> tokens = interpretation.getTokens();
-//        if (tokens != null && !tokens.isEmpty()) {
-//            if (tokens.contains("love") || tokens.contains("cute")) {
-//                interpretation.setEmotion(RoboyEmotion.SHY);
-//            } else if (tokens.contains("munich") || tokens.contains("robotics")) {
-//                interpretation.setEmotion(RoboyEmotion.SMILE_BLINK);
-//            } else if (tokens.contains("left")) {
-//                interpretation.setEmotion(RoboyEmotion.LOOK_LEFT);
-//            } else if (tokens.contains("right")) {
-//                interpretation.setEmotion(RoboyEmotion.LOOK_RIGHT);
-//            } else if (tokens.contains("cat") || tokens.contains("cats")) {
-//                interpretation.setEmotion(RoboyEmotion.CAT_EYES);
-//            } else {
-//                interpretation.setEmotion(RoboyEmotion.NEUTRAL);
-//            }
-//
-//            if (interpretation.isRoboy()) {
-//                interpretation.setEmotion(RoboyEmotion.SMILE_BLINK);
-//            }
-//        }
-//        return interpretation;
     }
 
     public static double cosineSimilarity(double[] vectorA, double[] vectorB) {
