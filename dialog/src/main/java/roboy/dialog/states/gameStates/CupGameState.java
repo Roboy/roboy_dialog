3d916e59ab3fbf9378dc87cb9102facd033c685f
missxa <aliona.kharchenko@gmail.com>
2018-10-08 22:41:23 +0200

    with json phrases for cupgame

--- dialog/src/main/java/roboy/dialog/states/gameStates/CupGameState.java
+++ dialog/src/main/java/roboy/dialog/states/gameStates/CupGameState.java
@@ -13,6 +13,7 @@ import roboy.logic.Inference;
 import roboy.logic.StatementInterpreter;
 import roboy.talk.PhraseCollection;
 import roboy.talk.Verbalizer;
+import roboy.util.QAJsonParser;
 import roboy.util.RandomList;
 
 import java.util.*;
@@ -22,6 +23,8 @@ import static roboy.util.FileLineReader.readFile;
 
 public class CupGameState extends State {
 
+    private final String PHRASE_FILE_PARAMETER = "cupGamePhrases";
+    private QAJsonParser phrases;
     private final static List<String> EXIT_REQUEST = Arrays.asList("bored", "stop", "something else", "different", "boring");
     private final static List<String> READY = Arrays.asList("done", "ready", "let's go", "play");
     private final static String TRANSITION_EXIT = "exit";
@@ -36,24 +39,29 @@ public class CupGameState extends State {
     }
 
 
-    private final static String FIND_CUPS = "find_cups";
-    private final static String GOTOCUP1 = "cup_1";
-    private final static String GOTOCUP2 = "cup_2";
-    private final static String FINISHED = "finished";
+    private final static String FIND_CUPS = "FIND_CUPS";
+    private final static String GOTOCUP1 = "GOTOCUP1";
+    private final static String GOTOCUP2 = "GOTOCUP2";
+    private final static String GOTOCUP3 = "GOTOCUP3";
+    private final static String FINISHED = "FINISHED";
 
     private CupGamePhase phase = CupGamePhase.SHUFFLE;
 
     public CupGameState(String stateIdentifier, StateParameters params) {
 
         super(stateIdentifier, params);
+        String phraseListPath = params.getParameter(PHRASE_FILE_PARAMETER);
+        LOGGER.info(" -> Phrases path: " + phraseListPath);
+        phrases = new QAJsonParser(phraseListPath);
     }
 
     @Override
     public Output act() {
-        switch (phase){
-            case SHUFFLE:
-                return Output.say("you shall shuffle the cups, my friend. let me know when you are ready");
-            case SCAN:
+//        switch (phase){
+//            case SHUFFLE:
+//                return Output.say(phrases.getQuestions(phase.toString()).getRandomElement());
+//            case SCAN:
+        if (phase == CupGamePhase.SCAN) {
                 // TODO get response from the state machine
 //                if (getRosMainNode().StartCupGameSmach()) {
                     // if smach start is successful
@@ -63,13 +71,21 @@ public class CupGameState extends State {
                     while(!currentSmachState.equals(FINISHED)) {
                         currentSmachState = getContext().CUP_GAME_SMACH_STATE_MSG.getValue();
                         if (!currentSmachState.equals(prevState)) {
+                            LOGGER.info("[CupGameState] commenting: " + currentSmachState);
                             switch (getContext().CUP_GAME_SMACH_STATE_MSG.getValue()) {
                                 case FIND_CUPS:
-                                    getRosMainNode().SynthesizeSpeech("i am looking for cups");
+                                    getRosMainNode().SynthesizeSpeech(
+                                            phrases.getQuestions(getContext().CUP_GAME_SMACH_STATE_MSG.
+                                                    getValue()).
+                                                    getRandomElement());
                                     break;
                                 case GOTOCUP1:
                                     getRosMainNode().ShowEmotion(RoboyEmotion.HYPNO_COLOUR_EYES);
-                                    getRosMainNode().SynthesizeSpeech("wubba lubba dub dub, doing magic for this cup");
+
+                                    getRosMainNode().SynthesizeSpeech(
+                                            phrases.getQuestions(getContext().CUP_GAME_SMACH_STATE_MSG.
+                                                    getValue()).
+                                                    getRandomElement());
                                     break;
                             }
 
@@ -77,49 +93,70 @@ public class CupGameState extends State {
                         }
 //                    }
                 }
-                return Output.say("so are you ready to hear the oracle's prophecy?");
-            case GUESS:
-                return Output.say("I think I found it there. So do I win now?");
-            case OFFER_REPEAT:
-                return Output.say("wanna play again?");
-            default:
-                return Output.sayNothing();
+//                return Output.say(phrases.getQuestions(phase.toString()).getRandomElement());
+//            case GUESS:
+//                return Output.say("I think I found it there. So do I win now?");
+//            case OFFER_REPEAT:
+//                return Output.say("wanna play again?");
+//            default:
+//                return Output.sayNothing();
         }
+        return Output.say(phrases.getQuestions(phase.toString()).getRandomElement());
 
     }
 
     @Override
     public Output react(Interpretation input) {
         exitOnRequest(input.getSentence());
-
+        Boolean pickSuccessAnswer = true;
+        CupGamePhase next = CupGamePhase.EXIT;
         switch (phase) {
             case SHUFFLE:
                 if (StatementInterpreter.isFromList(input.getSentence(),READY)) {
-                    phase = CupGamePhase.SCAN;
-                    return Output.say("Alright. Let me check with my superpower soli where did you hide the ball");
+                    next = CupGamePhase.SCAN;
+                    break;
+//                    return Output.say("Alright. Let me check with my superpower soli where did you hide the ball");
                 }
                 else {
-                    return Output.say("Hm, seems you are not done yet. I give you another try");
+                    pickSuccessAnswer = false;
+                    break;
+//                    return Output.say("Hm, seems you are not done yet. I give you another try");
                 }
             case SCAN:
-                phase = CupGamePhase.GUESS;
-                return Output.useFallback();
+                next = CupGamePhase.GUESS;
+                break;
+//                return Output.useFallback();
             case GUESS:
-                phase = CupGamePhase.OFFER_REPEAT;
-                return Output.say("ha-ha.");
+                next = CupGamePhase.OFFER_REPEAT;
+                break;
+//                return Output.say("ha-ha.");
             case OFFER_REPEAT:
                 if (StatementInterpreter.isFromList(input.getSentence(),Verbalizer.consent)) {
-                    phase = CupGamePhase.SHUFFLE;
-                    return Output.say("Here we go again then!");
+                    next = CupGamePhase.SHUFFLE;
+//                    return Output.say("Here we go again then!");
                 }
-                else
-                    phase = CupGamePhase.EXIT;
-                    return Output.say("I believe you've had enough of my magic");
-            case EXIT:
-                return Output.say("Wanna stop playing? Sure, if you say so.");
-            default:
-                return Output.useFallback();
+                else {
+                    next = CupGamePhase.EXIT;
+                    pickSuccessAnswer = false;
+                }
+                break;
+//                    return Output.say("I believe you've had enough of my magic");
+//            case EXIT:
+//                return Output.say("Wanna stop playing? Sure, if you say so.");
         }
+
+        Output answer;
+
+        try {
+            answer = Output.say(pickSuccessAnswer ? phrases.getSuccessAnswers(phase.toString()).getRandomElement() :
+                    phrases.getFailureAnswers(phase.toString()).getRandomElement());
+        }
+        catch (Exception e) {
+            answer = Output.useFallback();
+        }
+
+        phase = next;
+        return answer;
 
     }
 
